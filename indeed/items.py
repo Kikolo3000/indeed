10c8e659e14c77efd50083a1dd462705deb6a04c
Kike <Kikolo3000@gmail.com>
2019-05-27 21:00:09 +0200

    adding files

---
+++ indeed/items.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class IndeedItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    title       = scrapy.Field(serializer=str)
+    company     = scrapy.Field(serializer=str)
+    place       = scrapy.Field(serializer=str)
+    description = scrapy.Field(serializer=str)
+    salary      = scrapy.Field(serializer=str)
+ 
+    pass
---
+++ indeed/spiders/spider.py
@@ -0,0 +1,44 @@
+# import sys
+# reload(sys)
+# sys.setdefaultencoding('utf8')
+
+import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from scrapy.exceptions import CloseSpider
+from indeed.items import *
+
+import  json
+import  unicodedata
+import  os
+
+#class IndeedSpider(scrapy.Spider):
+class IndeedSpider(CrawlSpider):
+	name = 'indeed'
+	item_count = 0
+	allowed_domain = ['https://www.indeed.es/', 'https://www.indeed.com/']
+	start_urls = ['https://www.indeed.com/jobs?q=e-learning']
+
+	rules = {
+		# Para cada item
+		Rule(LinkExtractor(allow = (), restrict_xpaths = ('//div[contains(@class, "pagination")]/a[last()]//a'))),
+		Rule(LinkExtractor(allow =(), restrict_xpaths = ('//div[contains(@class, "jobsearch-SerpJobCard")]//*[contains(@class, "title")]//*[contains(@class, "title")]')), callback = 'parse_item', follow = False)
+    }
+	def parse_item(self, response):        
+		item = IndeedItem()
+    #item = ItemLoader(item=IndeedItem(), response=response)
+		#info de producto
+		print ('\n\n element number %i in process \n \n' % self.item_count)
+		item['title'] = response.xpath('//h3/text()').extract_first()
+		item['company'] = response.xpath('//div[contains(@class,"jobsearch-JobInfoHeader")]//div[1]//div[1]/text()').extract()
+		item['company'] = response.xpath('//div[contains(@class,"jobsearch-JobInfoHeader-subtitle")]//div[contains(@class,"icl-u-lg-mr--sm")][1]//text()').extract()
+		item['place'] = response.xpath('//div[contains(@class,"jobsearch-JobInfoHeader")]//div[1]//div[last()]/text()').extract()
+		item['salary'] = response.xpath('//div[contains(@class,"jobsearch-JobMetadataHeader-item")]//text()').extract()
+		item['description'] = response.xpath('//div[contains(@class,"jobDescriptionText")]//text()').extract()
+		self.item_count += 1
+		if self.item_count > 20:
+			raise CloseSpider('item_exceeded')
+		print ('\n element number %i done \n' % self.item_count)
+		yield item
+        
+
